d550b3291cdee337ec70ecf7f9f6f87b28f77c78
Ganesh <ganesh.git7027@gmail.com>
2026-07-29 14:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (65.38%) - LeetSync

--- 137-single-number-ii/single-number-ii.cpp
+++ 137-single-number-ii/single-number-ii.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i=i+3){
-            if(nums[i]!=nums[i-1]) return nums[i-1];
+        int one=0;
+        int two=0;
+        for(int i=0;i<nums.size();i++){
+            one=(one^nums[i])&~two;
+            two=(two^nums[i])&~one;
+
         }
-        return nums[nums.size()-1];
+        return one;
     }
 };
